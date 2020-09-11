0f242330c853474073c7c0212d260b9658c3e43b
danielhmclean <daniel.h.mclean@gmail.com>
2020-09-11 11:50:48 -0400

    Code cleanup and Commenting
    Should be no logical change (untested)
    
    Adding comments and doing minor cleanup

--- python_server/osc_visca_server.py
+++ python_server/osc_visca_server.py
@@ -6,6 +6,20 @@ Description:
     Python Script to intercept OSC commands from programs like open source control
     and translate them into VISCA commands for a Birddog P200 cammera.
     
+    Usage:
+        This is a server that needs to run in the background to translate
+        OSC commands to birddog Cameras
+        Run in dedicated terminal (not in some IDE like spyder)
+        Make sure IP addresses are correct in this file
+        
+        TODO: 
+          -Focus control
+          -P200 Color settings (RESTful API)
+          -Config JSON file to house IP configurations
+          -Recieve Info from VISCA
+          -Send Info to OSC        
+          
+    
     OSC Commands:
         /{camera ID}/{command}/ args[]
         
@@ -17,20 +31,32 @@ Description:
             zoom_wide_variable |args: zoom_speed
         
 '''
-# receive OSC messages and send VISCA control messages to camera (both UDP)
-
+# --------------------------------------------------------
+#  Libraries 
+# --------------------------------------------------------
+# --- External ---
 # pip3 install aiosc
-# https://pypi.org/project/aiosc/
-# https://github.com/artfwo/aiosc
 import asyncio # for receiving OSC
 import aiosc # for receiving OSC
 # pip3 install python-osc
-# https://pypi.org/project/python-osc/
 from pythonosc import udp_client # for sending OSC
+
+# --- Standard ---
 from math import floor  # for fader
 import socket
 import binascii  # for printing the visca messages
 
+# --------------------------------------------------------
+# OSC server and client Settings (Open Stage Control)
+# --------------------------------------------------------
+osc_receive_port = 8002
+serverOSC_ip = '192.168.50.183' # there must be a way to listen for this... maybe osc_address[0]
+osc_send_port = 9002
+
+# --------------------------------------------------------
+#  Camera Settings
+# TODO: Put in JSON file
+# --------------------------------------------------------
 ### VISCA sender (socket)
 camera_ip = '192.168.50.40'
 camipDic = {
@@ -38,51 +64,31 @@ camipDic = {
         "2":'192.168.50.28',
         "3":'192.168.50.111'}
 
-#camera_ip = '127.0.0.1'
+# Visca Port:
 camera_port = 52381
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
-
-### VISCA receiver
 buffer_size = 1024
+
+
+# --------------------------------------------------------
+# Visca Receiver Socket setup
+# TODO: Not working
+# --------------------------------------------------------
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) # IPv4, UDP
 s.bind(('', camera_port)) # for testing use the port one higher than the camera's port
 s.settimeout(1.0) # only wait for a response for 1 second
 
-### VISCA Commands (Payloads)
+# --------------------------------------------------------
+#  VISCA Commands (Payloads)
+# --------------------------------------------------------
+# --- Misc ---
 camera_on = '81 01 04 00 02 FF'
 information_display_off = '81 01 7E 01 18 03 FF'
+
+# --- Position Memory Commands ---
 memory_recall = '81 01 04 3F 02 0p FF' # p: Memory number (=0 to F)
 memory_set = '81 01 04 3F 01 0p FF' # p: Memory number (=0 to F)
-movement_speed = '01'
-'''
-pan_speed = '05'
-tilt_speed = '05'
-pan_up = '81 01 06 01 VV WW 03 01 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_down = '81 01 06 01 VV WW 03 02 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_left = '81 01 06 01 VV WW 01 03 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_right = '81 01 06 01 VV WW 02 03 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_up_left = '81 01 06 01 VV WW 01 01 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_up_right = '81 01 06 01 VV WW 02 01 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_down_left = '81 01 06 01 VV WW 01 02 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_down_right = '81 01 06 01 VV WW 02 02 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-pan_stop = '81 01 06 01 VV WW 03 03 FF'.replace('VV', str(pan_speed)).replace('WW', str(tilt_speed))
-'''
-pan_dictionary = {
-    'pan_up' : '81 01 06 01 VV WW 03 01 FF',
-    'pan_down' : '81 01 06 01 VV WW 03 02 FF',
-    'pan_left' : '81 01 06 01 VV WW 01 03 FF',
-    'pan_right' : '81 01 06 01 VV WW 02 03 FF',
-    'pan_up_left' : '81 01 06 01 VV WW 01 01 FF',
-    'pan_up_right' : '81 01 06 01 VV WW 02 01 FF',
-    'pan_down_left' : '81 01 06 01 VV WW 01 02 FF',
-    'pan_down_right' : '81 01 06 01 VV WW 02 02 FF'}
 
-# YYYY: Pan Position DE00 (−170 degree) to 2200 (170 degree) (CENTER 0000)
-# ZZZZ: Tilt Position EE00 (–90 degree) to 0400 (90 degree) (CENTER 0000)
-# for high speed VV = 18 and WW = 17
-pan_direct = '8x 01 06 02 18 17 0Y 0Y 0Y 0Y 0Z 0Z 0Z 0Z FF' # absolute position  
-pan_stop = '81 01 06 01 15 15 03 03 FF' # replaced VV and WW with 15
-pan_home = '81 01 06 04 FF'
-pan_reset = '81 01 06 05 FF'
+# --- Focus Commands ---
 focus_stop = '81 01 04 08 00 FF'
 focus_far = '81 01 04 08 02 FF'
 focus_near = '81 01 04 08 03 FF'
@@ -92,6 +98,8 @@ focus_direct = '81 01 04 48 0p 0q 0r 0s FF' #.replace('p', ) q, r, s
 focus_auto = '81 01 04 38 02 FF'
 focus_manual = '81 01 04 38 03 FF'
 focus_infinity = '81 01 04 18 02 FF'
+
+# --- Zoom Commands ---
 zoom_stop = '81 01 04 07 00 FF'
 zoom_tele = '81 01 04 07 02 FF'
 zoom_wide = '81 01 04 07 03 FF'
@@ -104,10 +112,11 @@ inquiry_lens_control = '81 09 7E 7E 00 FF'
 # response: 81 50 0p 0q 0r 0s 0H 0L 0t 0u 0v 0w 00 xx xx FF
 inquiry_camera_control = '81 09 7E 7E 01 FF'
 
-
-movement_speed = '05'
-pan_speed = movement_speed
-tilt_speed = movement_speed
+# --- Pan/Tilt Commands ---
+pan_stop = '81 01 06 01 15 15 03 03 FF' # replaced VV and WW with 15
+pan_home = '81 01 06 04 FF'
+pan_reset = '81 01 06 05 FF'
+# for high speed VV = 18 and WW = 17
 panDic = {
 'pan_up' : '81 01 06 01 VV WW 03 01 FF'        ,
 'pan_down' : '81 01 06 01 VV WW 03 02 FF'      ,
@@ -119,13 +128,15 @@ panDic = {
 'pan_down_right' : '81 01 06 01 VV WW 02 02 FF',
 'pan_stop' : '81 01 06 01 VV WW 03 03 FF'
 }
-
-
-# YYYY: Pan Position DE00 to 2200 (CENTER 0000)
-# ZZZZ: Tilt Position FC00 to 1200 (CENTER 0000)
+# YYYY: Pan Position 0xF92A to 0x06d6  (CENTER 0000)
+# ZZZZ: Tilt Position 0xfe80 to 0x0480 (CENTER 0000)
 #           pan_direct = '8x 01 06 02 18 17 0Y 0Y 0Y 0Y 0Z 0Z 0Z 0Z FF' # absolute position  
-pan_absolute_position = '81 01 06 02 VV WW 0Y1 0Y2 0Y3 0Y4 0Z1 0Z2 0Z3 0Z4 FF' #.replace('VV', str(VV)) #YYYY[0]
-#pan_relative_position = '81 01 06 03 VV WW 0Y 0Y 0Y 0Y 0Z 0Z 0Z 0Z FF'.replace('VV', str(VV))
+pan_absolute_position = '81 01 06 02 VV WW 0Y1 0Y2 0Y3 0Y4 0Z1 0Z2 0Z3 0Z4 FF' #Y is pan position, Z is tilt, VV is pan speed, WW is tilt speed
+
+# --------------------------------------------------------
+# Function convert absolute andgle 
+# to VISCA command for BIRDDOG P200
+# --------------------------------------------------------
 def panToHex(numP,numT):
     
     minPD = -175
@@ -159,6 +170,13 @@ def panToHex(numP,numT):
     return(hex(absP)[2:].zfill(4),hex(absT)[2:].zfill(4))
     
     
+# ==============================================================
+#  VISCA (TO Birddog P200s) 
+# ==============================================================
+# --------------------------------------------------------
+# Send Visca Command 
+# TODO: Cleanup, there only needs to be one
+# --------------------------------------------------------
 def send_message(message_string, camId = "1"):
     camera_ip = camipDic[camId]
     global sequence_number
@@ -197,26 +215,6 @@ def send_message(message_string, camId = "1"):
     received_message = 'test'
     return received_message
 
-def reset_sequence_number_function(camId = "1"):  # this should probably be rolled into the send_visca function
-    camera_ip = camipDic[camId]
-    reset_sequence_number_message = bytearray.fromhex('02 00 00 01 00 00 00 01 01')
-    s.sendto(reset_sequence_number_message,(camera_ip, camera_port))
-    global sequence_number
-    sequence_number = 1
-    print('Reset sequence number to', sequence_number)
-    try:
-        data = s.recvfrom(buffer_size)
-        received_message = binascii.hexlify(data[0])
-        #print('Received', received_message)
-        data = s.recvfrom(buffer_size)
-        received_message = binascii.hexlify(data[0])
-        #print('Received', received_message)
-        send_osc('reset_sequence_number', 1.0)
-    except socket.timeout: # s.settimeout(2.0) #above
-        received_message = 'No response from camera'
-        print(received_message)
-        send_osc('reset_sequence_number', 0.0)
-    return sequence_number
 def send_visca(message_string,camId="1"):
     camera_ip = camipDic[camId]
     global sequence_number
@@ -248,20 +246,62 @@ def send_visca(message_string,camId="1"):
     #return visca_message
     return received_message
 
-### OSC server and client
-osc_receive_port = 8002
-touchOSC_ip = '192.168.50.183' # there must be a way to listen for this... maybe osc_address[0]
-osc_send_port = 9002
+# --------------------------------------------------------
+# Reset Visca Sequence Number:
+# TODO: Check if this does anything or is necessary for
+#       Birddog P200
+# --------------------------------------------------------
+def reset_sequence_number_function(camId = "1"):  # this should probably be rolled into the send_visca function
+    camera_ip = camipDic[camId]
+    reset_sequence_number_message = bytearray.fromhex('02 00 00 01 00 00 00 01 01')
+    s.sendto(reset_sequence_number_message,(camera_ip, camera_port))
+    global sequence_number
+    sequence_number = 1
+    print('Reset sequence number to', sequence_number)
+    try:
+        data = s.recvfrom(buffer_size)
+        received_message = binascii.hexlify(data[0])
+        #print('Received', received_message)
+        data = s.recvfrom(buffer_size)
+        received_message = binascii.hexlify(data[0])
+        #print('Received', received_message)
+        send_osc('reset_sequence_number', 1.0)
+    except socket.timeout: # s.settimeout(2.0) #above
+        received_message = 'No response from camera'
+        print(received_message)
+        send_osc('reset_sequence_number', 0.0)
+    return sequence_number
 
+
+# ==============================================================
+#  OSC (From Open Stage Control)
+# ==============================================================
+# --------------------------------------------------------
+#  OSC Send 
+#  TODO: Untested/Unused... we should figure out
+#        feed back from P200 to Open Stage Control
+# --------------------------------------------------------
 def send_osc(osc_command, osc_send_argument):
     osc_message_to_send = '/1/' + osc_command
-    osc_client = udp_client.SimpleUDPClient(touchOSC_ip, osc_send_port)
+    osc_client = udp_client.SimpleUDPClient(serverOSC_ip, osc_send_port)
     osc_client.send_message(osc_message_to_send, osc_send_argument)
 
-### OSC receiving server
+
+# --------------------------------------------------------
+#  OSC Protocal Lambda Function 
+# --------------------------------------------------------
+def protocol_factory():
+    osc = aiosc.OSCProtocol({'//*': lambda osc_address, osc_path, *args: parse_osc_message(osc_address, osc_path, args)})
+    return osc
+
+# --------------------------------------------------------
+#  OSC Parse
+#  This is the main function to handle P200 Functions
+#  It translates OSC inpuit to VISCA output
+# --------------------------------------------------------
 def parse_osc_message(osc_address, osc_path, args):
-    global touchOSC_ip
-    touchOSC_ip = osc_address[0]
+    global serverOSC_ip
+    serverOSC_ip = osc_address[0]
     osc_path_list = osc_path.split('/')
     #print (osc_path_list)
     camId = osc_path_list[1]
@@ -350,14 +390,14 @@ def parse_osc_message(osc_address, osc_path, args):
     send_osc('SentMessageLabel', osc_command)
 
 
-## Start off by resetting sequence number
+# --------------------------------------------------------
+#  Main Routine
+# --------------------------------------------------------
+# Start off by resetting sequence number
 sequence_number = 1 # a global variable that we'll iterate each command, remember 0x0001
 reset_sequence_number_function()
 
-## Then start the OSC server to receive messages
-def protocol_factory():
-    osc = aiosc.OSCProtocol({'//*': lambda osc_address, osc_path, *args: parse_osc_message(osc_address, osc_path, args)})
-    return osc
+# Then start the OSC server to receive messages
 receive_loop = asyncio.get_event_loop()
 coro = receive_loop.create_datagram_endpoint(protocol_factory, local_addr=('0.0.0.0', osc_receive_port))
 transport, protocol = receive_loop.run_until_complete(coro)
