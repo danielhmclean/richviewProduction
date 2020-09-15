a90514fd69aaba3f238d44920f5de35cd42e4014
Daniel McLean <daniel.h.mclean@gmail.com>
2020-09-15 17:07:16 -0400

    Small changes to defaults

---
+++ run/server.bat
@@ -0,0 +1,8 @@
+:: Start Python Server
+start "OSC to VISCA Server" /B /D"%~dp0..\python_server" python osc_visca_server.py
+
+:: open stage control server. Asumes it is in parent directory
+start "OPEN STAGE CONTROL" /B /D"%~dp0..\open-stage-control-1.2.0-win32-x64" open-stage-control.exe -n -p9000 -o8000 -l "..\openStageControl\richview_main.json" --client-options zoom=0.85 -m loopMIDI:4,5 
+
+::Wait
+PAUSE
